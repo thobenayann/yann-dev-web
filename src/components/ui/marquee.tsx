46e5f26f55f65da6e27481d480f1aa8c7574bad4
thobenayann <thobena.yann@orange.fr>
2025-08-24 15:32:54 +0200

    technologies and refacto section

---
+++ src/components/ui/marquee.tsx
@@ -0,0 +1,56 @@
+import { cn } from '@/lib/utils';
+
+type MarqueeProps = {
+    className?: string;
+    reverse?: boolean;
+    pauseOnHover?: boolean;
+    children?: React.ReactNode;
+    vertical?: boolean;
+    repeat?: number;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function Marquee({
+    className,
+    reverse,
+    pauseOnHover = false,
+    children,
+    vertical = false,
+    repeat = 4,
+    ...props
+}: MarqueeProps) {
+    return (
+        <div
+            {...props}
+            className={cn(
+                'group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]',
+                {
+                    'flex-row': !vertical,
+                    'flex-col': vertical,
+                },
+                className
+            )}
+        >
+            {Array(repeat)
+                .fill(0)
+                .map((_, i) => (
+                    <div
+                        key={i}
+                        className={cn(
+                            'flex shrink-0 justify-around [gap:var(--gap)]',
+                            {
+                                'animate-[marquee_var(--duration)_linear_infinite] flex-row':
+                                    !vertical,
+                                'animate-[marquee-vertical_var(--duration)_linear_infinite] flex-col':
+                                    vertical,
+                                'group-hover:[animation-play-state:paused]':
+                                    pauseOnHover,
+                                '[animation-direction:reverse]': reverse,
+                            }
+                        )}
+                    >
+                        {children}
+                    </div>
+                ))}
+        </div>
+    );
+}
