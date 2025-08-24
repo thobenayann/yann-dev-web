a9642382d4cc695920421974c640388991d695d4
thobenayann <thobena.yann@orange.fr>
2025-08-24 18:09:30 +0200

    i18n refactoring

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
-import { locales } from './i18n/routing';
+import { defaultLocale, localePrefix, locales } from './i18n/config';
 
 export default createMiddleware({
-    // A list of all locales that are supported
-    locales: locales,
-
-    // Used when no locale matches
-    defaultLocale: 'en',
-
-    // Always show the locale in the URL
-    localePrefix: 'always',
+    locales,
+    defaultLocale,
+    localePrefix,
 });
 
 export const config = {
